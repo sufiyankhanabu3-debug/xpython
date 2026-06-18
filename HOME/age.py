c65ccce7e438fdb642293f59af2e5aa570204956
Abu Sufiyan <sufiyankhanabu3@gmail.com>
2026-06-18 19:07:18 +0530

    For Calculating you can drive or not

---
+++ HOME/age.py
@@ -0,0 +1,9 @@
+print(60*"=")
+print(60*"=")
+age=int(input("Enter your Age:  "))
+if age < 0 :
+	print("invalid age")
+elif age <=17 or age > 60 :
+	print("you can not drive bike")
+else :
+ print("you can drive")
